cae15df81b0a407cb381cc1c8161be5d499eb895
mcmyffin <dimitri.meier@haw-hamburg.de>
2015-11-05 16:59:39 +0100

    client fast fertig

--- Redis/src/redis/redis.java
+++ Redis/src/redis/redis.java
@@ -9,62 +9,78 @@ import java.util.Scanner;
  */
 public class redis {
 
-    private static Scanner sc = new Scanner(System.in);
-
+    private static String arg_help = "-help";
+    private static String arg_plz  = "-plz";
+    private static String arg_city = "-city";
+    private static String exit     = "exit";
+    private static String usage = "redis [ARGUMENT] <adress/hostname>\n"
+                                +"   <adress/hostname>  : IP-Adress/Hostname from Redis-Server\n\n"
+                                +"   "+arg_help+" : show Help usage\n"
+                                +"   "+arg_plz+"  : start interactive search for plz number\n"
+                                +"   "+arg_city+" : start interactive search for City value\n"
+                                +"   ------------------------------------------------\n"
+                                +"   "+exit+"  : close interactive search\n";
+                                
+    private static Scanner scanner = new Scanner(System.in);
     private static String read(){
 
-        return sc.nextLine();
+        return scanner.nextLine();
     }
 
     public static void main(String[] args) {
 
-        String adress = args[0];
-//        if(args.length != 2) System.exit(1);
-
-
+        /* wrong arguments count */
+        if(args.length != 2) {
+            showUsage(true);
+        }
+        
+        String argument = args[0];
+        String adress   = args[1];
+        
+        /* Create Redis-Client */
         Jedis client = new Jedis(adress);
 
-
-
-        for(String line = read(); !line.equals("exit"); line = read()){
-
-            if(line.equals("set")){
-
-                System.out.println("ADD KEY-Value to DB");
-                System.out.println("<KEY>");;
-
-                String key = read();
-
-                System.out.println("<VALUE>");
-
-                String value = read();
-
-                String returnVal = client.set(key,value);;
-                System.out.println(returnVal);
-
-
-//            }else if(line.equals("get")){
-//
-//                System.out.println("GET KEY-VALUE from DB");
-//                System.out.println("<KEY>");
-//
-//                String key = read();
-//
-//                try {
-//                    String value = new String(client.get(key));
-//                    System.out.println(value);
-//                } catch (RedisException e) {
-//                    System.out.println("ERROR: "+e.getMessage());
-//                }
-            }else{
-
-                System.out.println("UNSOPORTED COMMAND");
-                System.out.println("Commands:");
-                System.out.println("  get");
-                System.out.println("  set");
-                System.out.println("  exit");
-            }
+        
+        /* interactive search with PLZ */
+        if(argument.equals(arg_plz)){
+            print("interactive PLZ search activated");
+            searchForPLZ(client,scanner);
+            print("interactive PLZ search deactivated");
         }
-
+        
+        /* interactive search with City */
+        if(argument.equals(arg_city)){
+            print("interactive City search activated");
+            searchForCity(client, scanner);
+            print("interactive City search deactivated");
+        }
+        
+        showUsage(true);
+    }
+    
+    private static void showUsage(boolean withExit){
+        System.out.println(usage);
+        if(withExit) System.exit(1);
+    }
+    
+    private static void print(String message){
+        System.out.println(">> "+message);
+    }
+    
+    
+    private static void searchForPLZ(Jedis client, Scanner scanner){
+        
+        for(String line = read(); !line.equals(exit); line = read()){
+            print("NOT IMPLEMENTED !!!");
+            System.exit(2);
+        }
+    }
+    
+    private static void searchForCity(Jedis client, Scanner scanner){
+        
+        for(String line = read(); !line.equals(exit); line = read()){
+            print("NOT IMPLEMENTED !!!");
+            System.exit(2);
+        }
     }
 }
